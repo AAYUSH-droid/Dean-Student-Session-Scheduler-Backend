ffefb9ba93c6094cb8e86fb55ffea380bfd7c19c
aayush <sharma31aayush@gmail.com>
2023-12-07 16:09:06 +0530

    added CI

--- controllers/deanContoller.ts
+++ controllers/deanContoller.ts
@@ -11,7 +11,7 @@ export const registerDean = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const { name, password } = req.body;
+  const { universityId, password } = req.body;
 
   try {
     // Hash the password
@@ -20,16 +20,15 @@ export const registerDean = async (
     // Create a new dean in the database
     const newDean = await prisma.dean.create({
       data: {
-        universityId: name,
+        universityId: universityId,
         password: hashedPassword,
       },
     });
 
-    // Create a JWT token
     const token = jwt.sign(
       { universityId: newDean.universityId },
       process.env.JWT_SECRET as string,
-      { expiresIn: '1h' } // Adjust the expiration time as needed
+      { expiresIn: '1h' }
     );
 
     // Respond with the universityID and JWT token
@@ -37,5 +36,51 @@ export const registerDean = async (
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
+  }
+};
+
+//dean login
+export const deanLogin = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { universityId, password } = req.body;
+    const dean = await prisma.dean.findUnique({
+      where: { universityId },
+    });
+    if (!dean) {
+      res
+        .status(401)
+        .json({ success: false, message: 'Invalid universityId or password' });
+      return;
+    }
+    const passwordMatch = await bcrypt.compare(password, dean.password);
+
+    if (!passwordMatch) {
+      res
+        .status(401)
+        .json({ success: false, message: 'Invalid universityId or password' });
+      return;
+    }
+
+    //new JWT token on login
+    const authToken = jwt.sign(
+      { universityId },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: '1h',
+      }
+    );
+
+    res.json({
+      success: true,
+      message: 'Login successful',
+      authToken,
+      dean: {
+        id: dean.id,
+        universityId: dean.universityId,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
   }
 };
--- routes/deanRoute.ts
+++ routes/deanRoute.ts
@@ -1,8 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 const router = express.Router();
 
-import { registerDean } from '../controllers/deanContoller';
+import { registerDean, deanLogin } from '../controllers/deanContoller';
 
 router.route('/signup').post(registerDean);
+router.route('/login').post(deanLogin);
 
 module.exports = router;
